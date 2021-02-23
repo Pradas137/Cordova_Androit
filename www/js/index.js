05f3b55f7bf0bb866e62b4e5b6cd446bada07915
Pradas137 <pradas137@gmail.com>
2021-02-23 17:49:06 +0100

    mejoras

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,48 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
+const TASQUES = "tasques";
+
 function esborra(event){
-	var elem = event.target;
-	$(elem).parent().parent().remove();
+	//var elem = event.target;
+	//$(elem).parent().parent().remove();
+	var nameOfTask = $(this).parent('li').clone().children().remove().end().text().trim();
+	//find index in the task element
+	var index = tasques.indexOf(nameOfTask);
+	tasques.splice(index,1);
+	localStorage[TASQUES] = JSON.stringify(tasques);
 }
 
+function refresh_view(tasques){
+	console.log(tasques);
+	//Borrar tasks view
+	$("li").remove();
+	//Umplir dades view
+	var cont = 0;
+	for(const tasca of tasques){
+		$('ul').append("<li pos'"+cont+"' class='ui-last-child'><a class='ui-btn' href='#'>"+tasca+"<button class='deleteButton'>Elimina</button></a></li>");
+		cont++;
+	}
+}
+
+//configurar Button per afegir taskas
 function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
+    if(!localStorage.getItem(TASQUES)){
+    	localStorage.setItem(TASQUES, JSON.stringify([]));
+    }
 
 	$("#inputButton").click(function() {
         var text = $('#myText').val();
-        $('ul').append("<li class='ui-last-child'><a class='ui-btn' href='#'>"+text+"<button class='deleteButton'>Elimina</button></a></li>");
-		$('.deleteButton').click(esborra)
+        var tasques = JSON.parse(localStorage.getItem(TASQUES));
+		tasques.push(text);
+		localStorage.setItem("tasques",JSON.stringify(tasques));
+		refresh_view(tasques);
+		//$('.deleteButton').click(esborra)
     });
 
-
+	var dades = JSON.parse(localStorage.getItem(TASQUES));
+	refresh_view(dades);
 
 }
 
