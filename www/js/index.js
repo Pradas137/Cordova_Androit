0ec2b04866709cc471b82392b8a7183555a0edf8
Pradas137 <pradas137@gmail.com>
2021-02-23 19:14:46 +0100

    fin

--- www/js/index.js
+++ www/js/index.js
@@ -2,41 +2,43 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 const TASQUES = "tasques";
 
-function esborra(event){
-	//var elem = event.target;
-	//$(elem).parent().parent().remove();
-	var nameOfTask = $(this).parent('li').clone().children().remove().end().text().trim();
-	//find index in the task element
-	var index = tasques.indexOf(nameOfTask);
-	tasques.splice(index,1);
-	localStorage[TASQUES] = JSON.stringify(tasques);
-}
 
 function refresh_view(tasques){
 	console.log(tasques);
+
 	//Borrar tasks view
 	$("li").remove();
+
 	//Umplir dades view
 	var cont = 0;
 	for(const tasca of tasques){
-		$('ul').append("<li pos'"+cont+"' class='ui-last-child'><a class='ui-btn' href='#'>"+tasca+"<button class='deleteButton'>Elimina</button></a></li>");
+		$('ul').append("<li pos='"+cont+"'class='ui-li-has-alt ui-last-child'><a class='ui-btn' href='#'>"+tasca+"<button class='deleteButton'>Elimina</button></a></li>");
 		cont++;
 	}
+
+	$(".deleteButton").click(function() {
+		del = $(this).parent().attr('pos');
+		console.log(del)
+		borrar = JSON.parse(localStorage.getItem(TASQUES));
+		borrar.splice(del,1);
+		localStorage.setItem(TASQUES,JSON.stringify(borrar))
+		refresh_view(borrar);
+	});
 }
 
 //configurar Button per afegir taskas
 function onDeviceReady() {
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     //document.getElementById('deviceready').classList.add('ready');
-    if(!localStorage.getItem(TASQUES)){
-    	localStorage.setItem(TASQUES, JSON.stringify([]));
+    if(!localStorage.getItem("tasques")){
+    	localStorage.setItem("tasques", JSON.stringify([]));
     }
 
 	$("#inputButton").click(function() {
         var text = $('#myText').val();
         var tasques = JSON.parse(localStorage.getItem(TASQUES));
 		tasques.push(text);
-		localStorage.setItem("tasques",JSON.stringify(tasques));
+		localStorage.setItem(TASQUES,JSON.stringify(tasques));
 		refresh_view(tasques);
 		//$('.deleteButton').click(esborra)
     });
